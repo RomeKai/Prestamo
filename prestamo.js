03ac32f635c9aa7c35003ae866fbae4adec6fef3
Romeo L. Monfroglio <141967584+RomeKai@users.noreply.github.com>
2023-08-29 00:31:54 -0300

    Add files via upload

--- prestamo.js
+++ prestamo.js
@@ -1,4 +1,10 @@
 
+
+    
+    //CONFIRMS DE PRESTAMO
+
+function confirmar()
+{
 const acpbtn = document.querySelector(".acept-btn")
 const cancel = document.querySelector(".cancel-btn")
 
@@ -9,4 +15,40 @@ acpbtn.addEventListener("click", () => {
 cancel.addEventListener("click", () => {
     confirm("Esta seguro que quiere desechar este prestamo en BEEBANK")
 });
+}
+
+//CALCULADORA DE INTERES
+function loancalculator() {  
+    // Obtiene los datos de los input y los convierte en float
+    var valuep = Number(document.getElementById("loanamount").value);
+    var interest = Number(document.getElementById("anualinterest").value) / 100;
+    var timem = Number(document.getElementById("timeinmonths").value);
+
+    // Valida que los valores sean numéricos y no estén en blanco
+    if (isNaN(valuep) || isNaN(interest) || isNaN(timem)) {
+        alert("Por favor, ingrese valores numéricos válidos en todos los campos.");
+        return;
+    }
+
+    // El interés dividido por los 12 meses del año
+    var monthlyinterest = interest / 12;
+
+    // Calcula el pago mensual usando la fórmula correcta
+    var monthlypay = (valuep * monthlyinterest * Math.pow(1 + monthlyinterest, timem)) / (Math.pow(1 + monthlyinterest, timem) - 1);
+
+    // Calcula el pago total sumando el valor principal
+    var totalpayment = monthlypay + valuep;
+
+    // Mostrar en pantalla los resultados y redondearlos con toFixed
+    document.getElementById("valuep").innerHTML = valuep.toFixed(2);
+    document.getElementById("monthlypay").innerHTML = monthlypay.toFixed(2);
+    document.getElementById("totalpayment").innerHTML = totalpayment.toFixed(2);
+}
+
+
+
+
+
+
+
 
